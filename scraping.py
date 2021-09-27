68b7f16e02121db97168dff1aecd3bdf3edc3962
aryan3773 <61661726+aryan3773@users.noreply.github.com>
2021-09-27 23:05:26 +0530

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -4,18 +4,28 @@
 # In[1]:
 
 
-import requests
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
+# Importing Libraries
+
 import time
+import os
+from datetime import datetime
+from secedgar.filings import Filing, FilingType
+from secedgar.cik_lookup import get_cik_map
+time.sleep(10)
+# os.getcwd()
+
+import bs4 as bs
+import requests
+import pandas as pd
+import re
 time.sleep(10)
 
 
 # In[2]:
 
 
-import requests
+# To avoid 'user limit exceeded'
+
 import requests_random_user_agent
 
 s = requests.Session()
@@ -25,185 +35,288 @@ print(s.headers['User-Agent'])
 # In[3]:
 
 
-# Item 16 not present
-#r = requests.get('https://www.sec.gov/Archives/edgar/data/1288776/000165204416000012/goog10-k2015.htm')
+cik = get_cik_map()['ticker']
+print("Total number of companies are", len(cik))
 
-# doc_type = 10-K not found, use table info for finding summary
-#r = requests.get('https://www.sec.gov/Archives/edgar/data/320193/000032019320000096/aapl-20200926.htm')
-
-# Item 16 returns None as expected and returning texts for other items as well
-r = requests.get('https://www.sec.gov/Archives/edgar/data/320193/000032019318000145/0000320193-18-000145.txt')
-
-# Contains item 16
-#r = requests.get('https://www.sec.gov/Archives/edgar/data/0001163302/000116330221000013/x-20201231.htm')
-
-# doc_type = 10-K not found, use table info for finding summary, Item-16 not present
-#r = requests.get('https://www.sec.gov/Archives/edgar/data/1000045/000156459019023956/nick-10k_20190331.htm')
-
-# Another different type of doc, besides item-16 it has no table of content as well
-#r = requests.get('https://www.sec.gov/Archives/edgar/data/1000683/000121390019005351/f10k2018_blondertongue.htm')
-
-raw_10k = r.text
+# outputPath = "C:\\Users\\ARYAN\\Desktop\\9th Sem\\Project"
 
 
 # In[4]:
 
 
-print(raw_10k[0:2000])
+#  Downloading 10-K files using SEC Library
+
+'''
+start = time.time()
+
+total = 0
+
+for idx, ck in enumerate(list(cik.keys())[:11]):
+    print(idx, ck, cik[ck])
+    
+    temp = Filing(cik_lookup = list(cik.keys())[idx],
+                filing_type = FilingType.FILING_10K,
+                start_date = datetime(2016, 1, 1),
+                end_date = datetime(2022, 1, 1),
+                user_agent = 'aryanpcm@gmail.com')
+    
+    try:
+        print("Filings found")
+        temp.save(outputPath)
+        
+    except:
+        total += 1
+        print("No filings found")
+
+end = time.time()
+print('Total Time taken -----', round((end-start)/60, 2), 'minutes')'''
+
+
+# In[4]:
+
+
+list_of_companies = cik.keys()
+lookup_table = cik
 
 
 # In[5]:
 
 
-# Regex to find <DOCUMENT> tags
-doc_start_pattern = re.compile(r'<DOCUMENT>')
-doc_end_pattern = re.compile(r'</DOCUMENT>')
-# Regex to find <TYPE> tag prceeding any characters, terminating at new line
-type_pattern = re.compile(r'<TYPE>[^\n]+')
+# Checking the content of cik
+#cik
 
 
-# In[6]:
+# In[32]:
 
 
-# Create 3 lists with the span idices for each regex
+# Extracting relevant links
 
-### There are many <Document> Tags in this text file, each as specific exhibit like 10-K, EX-10.17 etc
-### First filter will give us document tag start <end> and document tag end's <start> 
-### We will use this to later grab content in between these tags
-doc_start_is = [x.end() for x in doc_start_pattern.finditer(raw_10k)]
-doc_end_is = [x.start() for x in doc_end_pattern.finditer(raw_10k)]
+year = 2020
+quarter = 'QTR1'
 
-### Type filter is interesting, it looks for <TYPE> with Not flag as new line, ie terminare there, with + sign
-### to look for any char afterwards until new line \n. This will give us <TYPE> followed Section Name like '10-K'
-### Once we have have this, it returns String Array, below line will with find content after <TYPE> ie, '10-K' 
-### as section names
-doc_types = [x[len('<TYPE>'):] for x in type_pattern.findall(raw_10k)]
+#get name of all filings 
+download = requests.get(f'https://www.sec.gov/Archives/edgar/full-index/{year}/{quarter}/master.idx').content
+download = download.decode("utf-8").split('\n')
 
 
 # In[7]:
 
 
-document = {}
-
-# Create a loop to go through each section type and save only the 10-K section in the dictionary
-for doc_type, doc_start, doc_end in zip(doc_types, doc_start_is, doc_end_is):
-    if doc_type == '10-K':
-        document[doc_type] = raw_10k[doc_start:doc_end]
+# Checking Content of download
+print(download[2001:2011])
 
 
-# In[8]:
+# In[33]:
 
 
-# display excerpt the document
-document['10-K'][0:500]
+# build the first part of the url
+
+filing = '10-K'
+list_of_links = []
+
+for item in download:
+    company = item
+    company = company.strip()
+    splitted_company = company.split('|')
+    if(len(splitted_company) == 5):
+        if(splitted_company[-3] == '10-K'):
+            url = splitted_company[-1]
+            list_of_links.append(url)
+        
+print('Total number of links captured are', len(list_of_links))
 
 
 # In[9]:
 
 
-# Write the regex
-regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(1A|1B|7A|7|8|16)\.{0,1})|(ITEM\s(1A|1B|7A|7|8|16))')
+# Sample URL creation
 
-# Use finditer to math the regex
-matches = regex.finditer(document['10-K'])
+url = list_of_links[5]
+print(url)
 
-# Write a for loop to print the matches
-for match in matches:
-    print(match)
+url2 = url.split('-') 
+url2 = url2[0] + url2[1] + url2[2]
+url2 = url2.split('.txt')[0]
+print(url2) #edgar/data/1326801/000132680120000076
+
+to_get_html_site = 'https://www.sec.gov/Archives/' + url
+data = requests.get(to_get_html_site).content
+data = data.decode("utf-8") 
+data = data.split('FILENAME>')
+# print(data[1])
+data = data[1].split('\n')[0]
+
+url_to_use = 'https://www.sec.gov/Archives/'+ url2 + '/' + data
+print(url_to_use)  # Returns a link like https://www.sec.gov/Archives/edgar/data/1001601/000149315219005484/form10-k.htm
 
 
 # In[10]:
 
 
-# Matches
-matches = regex.finditer(document['10-K'])
-
-# Create the dataframe
-test_df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
-
-test_df.columns = ['item', 'start', 'end']
-test_df['item'] = test_df.item.str.lower()
-
-# Display the dataframe
-test_df.head()
+r = requests.get('https://www.sec.gov/Archives/edgar/data/1001601/000149315219005484/form10-k.htm')
+raw_10k = r.text
 
 
 # In[11]:
 
 
-
-# Get rid of unnesesary charcters from the dataframe
-test_df.replace('&#160;',' ',regex=True,inplace=True)
-test_df.replace('&nbsp;',' ',regex=True,inplace=True)
-test_df.replace(' ','',regex=True,inplace=True)
-test_df.replace('\.','',regex=True,inplace=True)
-test_df.replace('>','',regex=True,inplace=True)
-
-# display the dataframe
-test_df.head()
-
-
-# In[12]:
-
-
-# Drop duplicates
-pos_dat = test_df.sort_values('start', ascending=True).drop_duplicates(subset=['item'], keep='last')
-
-# Display the dataframe
-pos_dat
-
-
-# In[13]:
-
-
-
-# Set item as the dataframe index
-pos_dat.set_index('item', inplace=True)
-
-# display the dataframe
-pos_dat
-
-
-# In[14]:
-
-
-# Get Item 1a
-item_1a_raw = document['10-K'][pos_dat['start'].loc['item1a']:pos_dat['start'].loc['item1b']]
-
-# Get Item 7
-item_7_raw = document['10-K'][pos_dat['start'].loc['item7']:pos_dat['start'].loc['item7a']]
-
-# Get Item 7a
-item_7a_raw = document['10-K'][pos_dat['start'].loc['item7a']:pos_dat['start'].loc['item8']]
-
-# Get Item 16
-item_16_raw = document['10-K'][pos_dat['start'].loc['item16']:pos_dat['start'].loc['item16']]
-
-
-# In[15]:
-
-
-print(item_1a_raw[0:1000])
-print('*'*50)
-
-item_1a_content = BeautifulSoup(item_1a_raw, 'lxml')
-print(item_1a_content.prettify()[0:1000])
-print('*'*50)
-
-# Final text after cleaning
-print(item_1a_content.get_text("\n\n")[0:5000])
+raw_10k
 
 
 # In[16]:
 
 
-print(item_16_raw[0:1000])
-print('*'*50)
+# Capturing the complete URL
 
-item_16_content = BeautifulSoup(item_16_raw, 'lxml')
-print(item_16_content.prettify()[0:1000])
-print('*'*50)
+url_list = []
 
-# Final text after cleaning
-print(item_16_content.get_text("\n\n")[0:1500])
+for i in range(len(list_of_links)):
+    print('Starting for', i)
+    url = list_of_links[i]
+    print(url) #edgar/data/1326801/0001326801-20-000076.txt
 
+    url2 = url.split('-') 
+    url2 = url2[0] + url2[1] + url2[2]
+    url2 = url2.split('.txt')[0]
+    #print(url2) #edgar/data/1326801/000132680120000076
+    try:
+        to_get_html_site = 'https://www.sec.gov/Archives/' + url
+        data = requests.get(to_get_html_site).content
+        data = data.decode("utf-8") 
+        data = data.split('FILENAME>')
+        #print(data[1])
+        data = data[1].split('\n')[0]
+        url_to_use = 'https://www.sec.gov/Archives/'+ url2 + '/'+data
+        #print(url_to_use)
+        url_list.append(url_to_use)
+        
+    except:
+        print("Can't form a link")
+print(len(url_list))
+
+
+# In[17]:
+
+
+# No specific point of termination
+print('Loop breaking at', i)
+
+
+# In[14]:
+
+
+
+
+
+# In[15]:
+
+
+# URL index at which loop terminated
+
+url = list_of_links[203]
+print(url)
+
+url2 = url.split('-') 
+url2 = url2[0] + url2[1] + url2[2]
+url2 = url2.split('.txt')[0]
+print(url2) #edgar/data/1326801/000132680120000076
+
+to_get_html_site = 'https://www.sec.gov/Archives/' + url
+data = requests.get(to_get_html_site).content
+data = data.decode("utf-8") 
+data = data.split('FILENAME>')
+# print(data[1])
+data = data[1].split('\n')[0]
+
+url_to_use = 'https://www.sec.gov/Archives/'+ url2 + '/' + data
+print(url_to_use)  # Returns a link like https://www.sec.gov/Archives/edgar/data/1001601/000149315219005484/form10-k.htm
+
+
+# In[15]:
+
+
+# Inspecting data for ith index
+print(data)
+
+
+# In[20]:
+
+
+import webbrowser
+#webbrowser.open("https://www.sec.gov/Archives/edgar/data/1102432/000147793219001357/vkin_10k.htm")
+for i in url_list[:5]:
+    webbrowser.open(i)
+
+
+# In[26]:
+
+
+# Types of files
+temp = []
+
+for item in download:
+    company = item
+    company = company.strip()
+    splitted_company = company.split('|')
+    if(len(splitted_company) == 5):
+        url = splitted_company[-3]
+        temp.append(url)
+
+
+# In[30]:
+
+
+temp = list(set(temp))
+#print(temp)
+
+for i in temp:
+    if('10-K' in i):
+        print(i)
+
+
+# In[36]:
+
+
+def get_links(year, quarter):
+    #get name of all filings 
+    download = requests.get(f'https://www.sec.gov/Archives/edgar/full-index/{year}/{quarter}/master.idx').content
+    download = download.decode("utf-8").split('\n')
+
+    # build the first part of the url
+    list_of_links = []
+    for item in download:
+        company = item
+        company = company.strip()
+        splitted_company = company.split('|')
+        if(len(splitted_company) == 5): # '1007499|IFS SECURITIES, INC|X-17A-5|2019-06-14|edgar/data/1007499/9999999997-19-005663.txt'
+            if(splitted_company[-3] == '10-K'):
+                url = splitted_company[-1]
+                list_of_links.append(url)
+
+    return len(list_of_links)
+
+
+# In[43]:
+
+
+years = [2015, 2016, 2017, 2018, 2019, 2020]
+quarters = ['QTR1', 'QTR2', 'QTR3', 'QTR4']
+total = 0
+
+for yr in years:
+    for qtr in quarters:
+        try:
+            p = get_links(yr, qtr)
+            print('The number of links in yr', yr, 'and Quarter', qtr, 'are', p)
+            total += p
+            
+        except:
+            print('Could not access for the year', yr, 'and Quarter', qtr)
+            
+print(total)
+
+
+# In[44]:
+
+
+print('The number of links in yr', 2017, 'and Quarter-3', 'are', get_links(2017, 'QTR3'))
